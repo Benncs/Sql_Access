74b6f63ba13a6b58bef87985952849e1d84ef1c0
Benncs <benjamin.casale@laposte.net>
2022-03-02 13:07:27 +0100

    Add getQuery for variadic column name

--- includes/sql_access.hpp
+++ includes/sql_access.hpp
@@ -8,9 +8,24 @@ class sqlAccess : public sqlManager{
     public :
         explicit sqlAccess(const sqlInfo& db);
         void getQuery(const std::string& query,const std::string& col);
+
+        template<typename ...queries>
+        void getQuery(const std::string& query,const std::string& col,const std::string&& ... queries);
     
     private:
         
 };
+
+template<typename ...queries>
+        void sqlAccess::getQuery(const std::string& query,const std::string& col,const std::string&& ... queries){
+            Query(query);
+            while(query_res->next()){
+                for(auto&& i :{...queries}){
+                    std::cout<<query_res->getString(i)<<" | ";
+                }
+                std::cout<<std::endl;
+            }  
+    }
+
 
 #endif 
